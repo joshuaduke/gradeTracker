832a3e1b81079e38854b6ab9d84b94e4800d1c66
Joshua Duke <dukejoshuae@gmail.com>
2021-12-27 18:36:56 -0500

    populate content for semester and semester edit pages

--- app.js
+++ app.js
@@ -7,8 +7,33 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
+    res.redirect('/semesters');
+})
+
+//Routes for semesters
+
+app.get('/semesters', (req,res)=>{
     res.render('semesters');
 })
+
+app.get('/edit_semesters', (req, res)=>{
+    res.render('semesters_edit');
+})
+
+app.post('/edit_semesters', (req, res)=>{
+    const semesterEditValue = req.body.semesterEdit;
+
+    if (semesterEditValue == 'Delete') {
+        console.log('IS DELETED');
+    } else if(semesterEditValue == 'Save'){
+        console.log('SAVED');
+    } else {
+        console.log('ERR');
+    }
+    res.redirect('/semesters')
+})
+
+
 
 
 app.listen(process.env.PORT || 3000, ()=>{
